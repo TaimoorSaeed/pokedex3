470ec5df71aab7cebdad31b978249d1fdd5c4b6f
TaimoorSaeed <taimoor.dev@gmail.com>
2017-08-28 04:42:08 +0500

    added collection view and delegates

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,35 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Taimoor Saeed on 28/08/2017.
+//  Copyright © 2017 Taimoor. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl : UILabel!
+    
+    var pokemon : Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+        
+    }
+    
+    func configureCell(pokemon: Pokemon){
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
